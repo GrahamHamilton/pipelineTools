1b4f3a4d9a5f609ff492055064a6df2e5adef2b1
Graham Hamilton <graham.hamilton@glasgow.ac.uk>
2019-10-16 12:35:30 +0100

    Add sample sort and prep

---
+++ R/prepare_samples.R
@@ -0,0 +1,60 @@
+#' Prepare samples
+#'
+#' @description Reads in the sample file names for the directory or directories. Ensures the files are in order, returns a dataframe with the paths
+#'              to the raw reads files, the trimmed reads files and the sample names. Can take paired end or single end data.
+#'
+#' @param path List of full paths to directory or directories containing the raw reads data
+#' @param patt List of suffix patterns for the raw reads data for forward and (optionally) reverse reads
+#' @param trimmed.reads Name of the directory for the quality and adapter trimmed reads
+#'
+#' @examples
+#' \dontrun{
+#' # Paths to reads
+#' reads.path <- c("/Path to reads 1",
+#'                 "/Path to reads 2")
+#'
+#' # Standard suffixes for reads files for forward and reverse files
+#' reads.patt.1 <- "*_R1_001.fastq.gz$"
+#' reads.patt.2 <- "*_R2_001.fastq.gz$"
+#'
+#' # Trimmed reads directory
+#' trimmed.reads <- "trimmed_reads"
+#'
+#' # Paired end data
+#' sample.dataframe <- prepare_samples(reads.path, c(reads.patt.1,reads.patt.1),trimmed.reads)
+#' mate1 <- as.character(sample.dataframe$reads.path.1)
+#' mate1.trim <- as.character(sample.dataframe$trimmed.reads.path.1)
+#' mate2 <- as.character(sample.dataframe$reads.path.2)
+#' mate2.trim <- as.character(sample.dataframe$trimmed.reads.path.2)
+#'
+#' # Single end data
+#' sample.dataframe <- prepare_samples(reads.path, reads.patt.1,trimmed.reads)
+#' mate1 <- as.character(sample.dataframe$reads.path.1)
+#' mate1.trim <- as.character(sample.dataframe$trimmed.reads.path.1)
+#'
+#' # Sample names
+#' sample.names <- as.character(sample.dataframe$sample.names)
+#' }
+#'
+#' @return Dataframe with sorted file paths and sample names
+#'
+#' @export
+#'
+
+prepare_samples<- function(path,patt,trimmed.reads){
+  reads1 <- as.data.frame(matrix(list.files(path = path, pattern = patt[1], full.names = TRUE, recursive = TRUE)))
+  colnames(reads1) <- c("reads.path.1")
+  reads1$trimmed.reads.path.1 <- paste(trimmed.reads,(basename(as.character(reads1$reads.path.1))),sep = "/")
+  reads1$sample.names <- unlist(lapply(strsplit(basename(as.character(reads1$reads.path.1)),"_"), `[[`, 1))
+
+  if (length(patt) > 1){
+    reads2 <- as.data.frame(matrix(list.files(path = path, pattern = patt[2], full.names = TRUE, recursive = TRUE)))
+    colnames(reads2) <- c("reads.path.2")
+    reads2$trimmed.reads.path.2 <- paste(trimmed.reads,(basename(as.character(reads2$reads.path.2))),sep = "/")
+    reads2$sample.names <- unlist(lapply(strsplit(basename(as.character(reads2$reads.path.2)),"_"), `[[`, 1))
+    df <- merge(reads1,reads2, by = "sample.names")
+    return(df)
+  }else{
+    return(reads1)
+  }
+}
---
+++ R/run_picard.R
@@ -0,0 +1,76 @@
+#' Run Picard
+#'
+#' @description Runs the Picard program. Curently only works for CollectRnaSeqMetrics command
+#'
+#' @param command Picard command to run, required
+#' @param input List of sorted bam files, required
+#' @param out.dir Name of the output directory, required
+#' @param refFlat Path to the refFlat file, required
+#' @param rRNA.intervals Path to the rRNAintrvals list file
+#' @param strand Strand-specific information, "FR" for first strand or "RF" for reverse strand
+#' @param sample.name List of the sample names, required
+#' @param picard Path to the Picard program, required
+#'
+#' @examples
+#' \dontrun{
+#' sorted.bam.files <- list.files(path = hisat2.alignments.dir, pattern = "sorted.bam$",
+#'                                full.names = TRUE, recursive = TRUE)
+#' sample_names <- unlist(lapply(strsplit(list.files(path = trimmed_reads_dir,
+#'                        pattern = "*_R1_001.fastq$",
+#'                        full.names = FALSE),"_"), `[[`, 1))
+#' picard.dir <- "picard"
+#' refFlat.file <- "refFlatBDGP6.txt"
+#'
+#' run_picard(command = "CollectRnaSeqMetrics",
+#'            input = sorted.bam.files,
+#'            out.dir = picard.dir,
+#'            refFlat = refFlat.file,
+#'            strand = strandedness,
+#'            sample.name = sample.names,
+#'            picard = picard.path)
+#' }
+#'
+#' @return A list with the HISAT2 commands
+#'
+#' @export
+#'
+
+run_picard <- function(command = NULL,
+                       input = NULL,
+                       out.dir = NULL,
+                       refFlat = NULL,
+                       rRNA.intervals = NULL,
+                       strand = NULL,
+                       sample.name = NULL,
+                       picard = NULL
+                       ){
+  sprintf("type -P %s &>//dev//null && echo 'Found' || echo 'Not Found'", picard)
+
+  # Set the additional arguments
+  args <- ""
+  # Strand
+  if (!is.null(strand)){
+    if (strand == "FR" || strand == "F"){
+      args <- paste(args,"STRAND=FIRST_READ_TRANSCRIPTION_STRAND",sep = " ")
+    }
+    if (strand == "RF" || strand == "R"){
+      args <- paste(args,"STRAND=SECOND_READ_TRANSCRIPTION_STRAND",sep = " ")
+    }
+  }
+  if (!is.null(rRNA.intervals)){
+    args <- paste(args,paste("RIBOSOMAL_INTERVALS=",rRNA.intervals,sep = ""),sep = " ")
+  }
+
+  # Create the output files list
+  picard.file <- paste(out.dir,paste(sample.name,"metrics.txt",sep = "."),sep = "/")
+
+  # Create the Picard commands
+  picard.run <- sprintf('java -jar %s %s I=%s O=%s REF_FLAT=%s %s',
+                        picard,command,input,picard.file,refFlat,args)
+
+  # Run the Picard commands
+  lapply(picard.run, function (cmd)  system(cmd))
+
+  # Return the list of Picard commands
+  return(picard.run)
+}
