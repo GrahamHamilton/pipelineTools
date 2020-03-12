2e6ced2b888d717c28eb155c450c199dd256e5a8
Graham Hamilton <graham.hamilton@glasgow.ac.uk>
2020-03-12 12:57:54 +0000

    Minor changes to Deeptools module

--- R/run_deeptools.R
+++ R/run_deeptools.R
@@ -10,18 +10,18 @@
 #' @param output Name of output direcotry
 #' @param sample.names list of the sample name
 #' @param gtf Path to the gtf file
-#' @param before.region Distance upstream of the reference-point selected,
-#'   default 500
-#' @param region.bases Distance in bases to which all regions will be fit,
-#'   default 1000
+#' @param before.region Distance upstream of the reference-point selected
+#' @param region.bases Distance in bases to which all regions will be fit
 #' @param after.region Distance downstream of the reference-point
-#'   selected,default 1500
+#'   selected
+#' @param plotType Type of plot can select lines, fill, se, std, overlapped_lines or heatmap
+#' @param kmeans Number of kmeans clusters to compute.
 #' @param threads Number of threads for each instance of deeptools to use,
 #'   default set to 10
 #' @param parallel Run in parallel, default set to FALSE
 #' @param cores Number of cores/threads to use for parallel processing, default
 #'   set to 4
-#' @param deeptools.path Path to the where the deeptools programs are sorted
+#' @param deeptools Path to the where the deeptools programs are sorted
 #'   (usually /usr/local/bin), required
 #' @param version Returns the version number
 #'
@@ -49,7 +49,7 @@
 #'                                input = input,
 #'                                output = output,
 #'                                sample.names = sample.names,
-#'                                deeptools.path = path)
+#'                                deeptools = path)
 #' deeptools.cmd
 #'
 #' # Bigwig to matrix
@@ -66,7 +66,7 @@
 #'                                gtf = gtf,
 #'                                # parallel = TRUE,
 #'                                # cores = 6,
-#'                                deeptools.path = path)
+#'                                deeptools = path)
 #' deeptools.cmd
 #'
 #' # plot heatmap
@@ -81,7 +81,7 @@
 #'                                sample.names = output.names,
 #'                                # parallel = TRUE,
 #'                                # cores = 6,
-#'                                deeptools.path = path)
+#'                                deeptools = path)
 #' deeptools.cmd
 #' }
 #' @export
@@ -91,38 +91,73 @@ run_deeptools <- function(command = NULL,
                           output = NULL,
                           sample.names = NULL,
                           gtf = NULL,
-                          before.region = 500,
-                          region.bases = 5000,
-                          after.region = 1500,
+                          before.region = NULL,
+                          region.bases = NULL,
+                          after.region = NULL,
+                          plotType = NULL,
+                          kmeans = NULL,
                           threads = 10,
                           parallel = FALSE,
                           cores = 4,
-                          deeptools.path = NULL,
+                          deeptools = NULL,
                           version = FALSE){
   # Version
   if (isTRUE(version)){
     deeptools.run <- sprintf('%s%s --version',
-                             deeptools.path,command)
+                             deeptools,command)
     result <- system(deeptools.run, intern = TRUE)
     return(result)
   }
 
+  # Set the additional arguments
+  args <- ""
+  # Before region
+  if (!is.null(before.region)){
+    args <- paste(args,"--beforeRegionStartLength",before.region,sep = " ")
+  }
+  # Region bases
+  if (!is.null(region.bases)){
+    args <- paste(args,"--regionBodyLength",region.bases,sep = " ")
+  }
+  # After region
+  if (!is.null(after.region)){
+    args <- paste(args,"--afterRegionStartLength",after.region,sep = " ")
+  }
+  # Plot type
+  if (!is.null(plotType)){
+    args <- paste(args,"--plotType",plotType,sep = " ")
+  }
+  # K means
+  if (!is.null(kmeans)){
+    args <- paste(args,"--kmeans",kmeans,sep = " ")
+  }
   # Bam to BigWig
   if (command == "bamCoverage"){
     deeptools.run <- sprintf('%s%s --numberOfProcessors %s --bam %s --outFileName %s',
-                             deeptools.path,command,threads,input,file.path(output,(paste(sample.names,"bw",sep = ".")),fsep = .Platform$file.sep))
+                             deeptools,command,threads,input,file.path(output,(paste(sample.names,"bw",sep = ".")),fsep = .Platform$file.sep))
   }
 
   # Compute Matrix
   if (command == "computeMatrix"){
-      deeptools.run <- sprintf('%s%s scale-regions --numberOfProcessors %s --scoreFileName %s --regionsFileName %s --outFileName %s --beforeRegionStartLength %s --regionBodyLength %s --afterRegionStartLength %s',
-                               deeptools.path,command,threads,input,gtf,file.path(output,(paste(sample.names,"mat","gz",sep = ".")),fsep = .Platform$file.sep),before.region,region.bases,after.region)
+      deeptools.run <- sprintf('%s%s scale-regions --numberOfProcessors %s --scoreFileName %s --regionsFileName %s --outFileName %s %s',
+                               deeptools,command,threads,input,gtf,file.path(output,(paste(sample.names,"mat","gz",sep = ".")),fsep = .Platform$file.sep),args)
   }
 
   # plotHeatmap
   if (command == "plotHeatmap"){
     deeptools.run <- sprintf('%s%s --matrixFile %s --outFileName %s',
-                             deeptools.path,command,input,file.path(output,(paste(sample.names,"heatmap","png",sep = ".")),fsep = .Platform$file.sep))
+                             deeptools,command,input,file.path(output,(paste(sample.names,"heatmap","png",sep = ".")),fsep = .Platform$file.sep))
+  }
+
+  # profile plots
+  if (command == "plotProfile"){
+    if (plotType == "heatmap"){
+      deeptools.run <- sprintf('%s%s --matrixFile %s --outFileName %s --perGroup %s',
+                               deeptools,command,input,file.path(output,(paste(sample.names,"profile","heatmap","png",sep = ".")),fsep = .Platform$file.sep),args)
+    }else{
+      deeptools.run <- sprintf('%s%s --matrixFile %s --outFileName %s --perGroup %s',
+                               deeptools,command,input,file.path(output,(paste(sample.names,"profile","png",sep = ".")),fsep = .Platform$file.sep),args)
+    }
   }
 
   # Run the deeptools commands
